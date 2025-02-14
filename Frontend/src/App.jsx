21bd039461efbee535433e98be0a70d013e327dd
chirag <sharmachirag2899@gmail.com>
2025-02-14 09:15:00 +0530

    Editor changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import "prismjs/themes/prism-funky.css";
-import Prism from "prismjs";
-import Editor from "react-simple-code-editor";
+import React, { useState ,useEffect} from "react";
 import axios from "axios";
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
+
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { dracula } from "@uiw/codemirror-theme-dracula";
 
 const App = () => {
     const [code, setCode] = useState(`function findSum(arr) {
@@ -34,30 +35,43 @@ console.log(findSum(arr));
         setLoading(false);
     };
 
+    const minLines = 37;
+    const emptyLines = "\n";
+
+    useEffect(() => {
+        const lines = code.split("\n").length;
+        if (lines < minLines) {
+            setCode((prev) => prev + emptyLines.repeat(minLines - lines));
+        }
+    }, [code]);
+    
     return (
         <div className="h-screen w-screen flex flex-col md:flex-row p-6 gap-6 bg-zinc-800 ">
-            <div className="relative flex-1 min-h-[50%] md:min-h-full p-4 rounded-[0.7rem] bg-zinc-950 shadow-lg overflow-auto border border-zinc-500">
-                <h1 className="text-white font-semibold text-3xl text-center mb-8 mt-2 tracking-wide">
+            <div className="relative flex-1 min-h-[50%] md:min-h-full p-4 rounded-[0.7rem] bg-zinc-950 shadow-lg overflow-auto border border-zinc-500 flex flex-col">
+                <h1 className="text-white font-semibold text-lg md:text-3xl text-center mb-4 md:mb-8 mt-2 tracking-wide">
                     ✨ Code Playground - Try Out Some JavaScript 💻
                 </h1>
-                <Editor
-                    value={code}
-                    onValueChange={setCode}
-                    highlight={(code) =>
-                        Prism.highlight(code, Prism.languages.javascript, "javascript")
-                    }
-                    padding={12}
-                    className="w-full text-white font-mono text-base"
-                    style={{ fontSize: 18 }}
-                />
 
-                <button
-                    className="absolute bottom-5 right-7 px-5 py-2 rounded-[0.7rem] text-[1.2rem] bg-white text-black shadow-md hover:bg-gray-200 transition"
-                    onClick={reviewCode}
-                    disabled={loading}
-                >
-                    {loading ? "Processing..." : "Review Code"}
-                </button>
+                <div className="flex-1 overflow-auto ">
+                    <CodeMirror
+                        value={code}
+                        extensions={[javascript()]}
+                        onChange={(value) => setCode(value)}
+                        theme={dracula}
+                        basicSetup={{ lineNumbers: true }}
+                        className="w-full h-full border border-zinc-600 rounded-lg overflow-auto text-[15px]"
+                    />
+                </div>
+
+                <div className="absolute bottom-7 right-7">
+                    <button
+                        className="px-[15px] py-[7px] rounded-[0.7rem] text-[1.1rem] bg-white text-black shadow-md hover:bg-gray-200 transition"
+                        onClick={reviewCode}
+                        disabled={loading}
+                    >
+                        {loading ? "Processing..." : "Review Code"}
+                    </button>
+                </div>
             </div>
 
             <div className="flex-1 min-h-[50%] md:min-h-full bg-neutral-900 rounded-[0.7rem] shadow-lg text-zinc-400 p-5 text-xl overflow-auto leading-relaxed whitespace-pre-wrap">
